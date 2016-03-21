34c968ad42b73ac20e339b1173cb7d4de7a20125
Floriane Sapin <floriane.sapin@he-arc.ch>
2016-03-21 12:06:18 +0100

    Implémentation point 4 Série 7 Redirection avec un dispatcher

--- src/java/DisplayMessage.java
+++ src/java/DisplayMessage.java
@@ -6,10 +6,12 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -28,20 +30,16 @@ public class DisplayMessage extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DisplayMessage</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DisplayMessage at " + request.getContextPath() + "</h1>");
-            out.println("<p> Hello world</p>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+            HttpSession session = request.getSession(true);
+            RequestDispatcher dispatcher; 
+            if (session.getAttribute("prenom") == null){
+                dispatcher = request.getRequestDispatcher("AskNameToStoreToSession");
+            } else {
+                dispatcher = request.getRequestDispatcher("DisplayInformations");
+            }
+            
+            dispatcher.forward(request, response);
+            
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
